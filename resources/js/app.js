6155aca0b5d29251f80e47da76e15e3fa7d5059f
maartenvanderbeeken <maartenvanderbeeken@hotmail.com>
2023-11-24 07:50:34 +0000

    FAQ page created

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 import './bootstrap';
+
+document.addEventListener('DOMContentLoaded', function() {
+    const accordionButtons = document.querySelectorAll('[data-accordion="collapse"] [data-accordion-target]');
+
+    accordionButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const targetId = this.getAttribute('data-accordion-target');
+            const targetElement = document.querySelector(targetId);
+
+            if (targetElement) {
+                const expanded = this.getAttribute('aria-expanded') === 'true';
+                this.setAttribute('aria-expanded', !expanded);
+                targetElement.classList.toggle('hidden');
+            }
+        });
+    });
+});
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('info', function () {
+    return view('info');
+});
+
+Route::get('faq', function () {
+    return view('faq');
+});
